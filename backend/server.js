2aed374f252b2024899cb945a5b1a5a5556a2eb4
Shakti Shrestha <75934459+ShaktiShresth@users.noreply.github.com>
2026-05-26 18:14:20 +0545

    1- user routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -3,9 +3,17 @@ import dotenv from "dotenv";
 
 import connectMongoDB from "./db/connectMongoDB.js";
 import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
 import cookieParser from "cookie-parser";
+import { v2 as cloudinary } from "cloudinary";
 
 dotenv.config();
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 const PORT = process.env.PORT || 5000;
 
@@ -16,6 +24,7 @@ app.use(express.urlencoded({ extended: true })); //parse form data
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
 
 app.listen(PORT, () => {
   console.log("Twitter/X's server is running on port:", PORT);
