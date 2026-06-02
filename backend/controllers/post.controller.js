1b1fa8b8b4ce0febceb3def07a697e48fb5621ed
Shakti Shrestha <75934459+ShaktiShresth@users.noreply.github.com>
2026-06-02 16:41:50 +0545

    3- Added like post mutation

--- backend/controllers/post.controller.js
+++ backend/controllers/post.controller.js
@@ -101,7 +101,11 @@ export const likeUnlikePost = async (req, res) => {
       // unlike post
       await Post.updateOne({ _id: postId }, { $pull: { likes: userId } });
       await User.updateOne({ _id: userId }, { $pull: { likedPosts: postId } });
-      return res.status(200).json({ message: "Post unliked successfully!" });
+
+      const updatedLikes = post.likes.filter(
+        (id) => id.toString() !== userId.toString(),
+      );
+      return res.status(200).json(updatedLikes);
     } else {
       // like post
       post.likes.push(userId);
@@ -115,7 +119,8 @@ export const likeUnlikePost = async (req, res) => {
       });
       await notification.save();
 
-      return res.status(200).json({ message: "Post liked successfully!" });
+      const updatedLikes = post.likes;
+      return res.status(200).json(updatedLikes);
     }
   } catch (error) {
     console.log("Error in likeUnlikePost controller:", error.message);
