e0e60e11e5bafbfe2a2f827d4eb0a8ba805ec51a
Shakti Shrestha <75934459+ShaktiShresth@users.noreply.github.com>
2026-06-03 16:10:33 +0545

    3- Added comment on post mutation

--- frontend/src/components/common/Post.jsx
+++ frontend/src/components/common/Post.jsx
@@ -9,11 +9,20 @@ import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { toast } from "react-hot-toast";
 
 import LoadingSpinner from "./LoadingSpinner";
+import { formatPostDate } from "../../utils/date";
 
 const Post = ({ post }) => {
 	const [comment, setComment] = useState("");
 	const { data: authUser } = useQuery({ queryKey: ["authUser"] });
 	const queryClient = useQueryClient();
+	
+	const postOwner = post.user;
+	const isLiked = post.likes.includes(authUser._id);
+
+	const isMyPost = authUser?._id === post.user._id;
+
+	const formattedDate = formatPostDate(post.createdAt);
+
 	const {mutate: deletePost, isPending:isDeleting} = useMutation({
 		mutationFn: async () => {
 			try {
@@ -70,14 +79,51 @@ const Post = ({ post }) => {
 		}
 	});
 
-	const postOwner = post.user;
-	const isLiked = post.likes.includes(authUser._id);
+	const {mutate: commentPost, isPending: isCommenting} = useMutation({
+		mutationFn: async () => {
+			try {
+				const res = await fetch(`/api/posts/comment/${post._id}`, {
+					method: "POST",
+					headers: {
+						"Content-Type": "application/json",
+					},
+					body: JSON.stringify({ text: comment }),
+				});
+				const data = await res.json();
+				if (!res.ok) {
+					throw new Error(data.error || "Something went wrong!");
+				}
+				return data;
+			} catch (error) {
+				throw new Error(error, { cause: error });
+			}
+		},
+		onSuccess: (updatedPost) => {
+			toast.success("Comment added successfully.");
+			setComment("");
 
-	const isMyPost = authUser?._id === post.user._id;
+		// Update cache directly - similar to likePost
+        queryClient.setQueryData(["posts"], (oldData) => {
+            if (!oldData) return oldData;
+            
+            return oldData.map((p) => {
+                if (p._id === post._id) {
+                    // Replace the old post with the updated one from backend
+                    return updatedPost;
+                }
+                return p;
+            });
+        });
 
-	const formattedDate = "1h";
+		// Close modal after commenting
+        const modal = document.getElementById(`comments_modal${post._id}`);
+        if (modal) modal.close();
 
-	const isCommenting = true;
+		},
+		onError: (error) => {
+			toast.error(error.message);
+		},
+	});
 
 	const handleDeletePost = () => {
 		deletePost();
@@ -85,6 +131,9 @@ const Post = ({ post }) => {
 
 	const handlePostComment = (e) => {
 		e.preventDefault();
+		
+		if(isCommenting) return;
+		commentPost();
 	};
 
 	const handleLikePost = () => {
