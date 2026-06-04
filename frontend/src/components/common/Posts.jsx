9563e35063663558c4a7ef27649eb0f3e4550227
Shakti Shrestha <75934459+ShaktiShresth@users.noreply.github.com>
2026-06-04 09:08:15 +0545

    3- Fetched profile page posts & user info

--- frontend/src/components/common/Posts.jsx
+++ frontend/src/components/common/Posts.jsx
@@ -3,13 +3,17 @@ import PostSkeleton from "../skeletons/PostSkeleton";
 import { useQuery } from "@tanstack/react-query";
 import { useEffect } from "react";
 
-const Posts = ({feedType }) => {
+const Posts = ({feedType, username, userId }) => {
 	const getPostEndpoint = () =>{
 		switch (feedType) {
 			case "forYou":
 				return "/api/posts/all";
 			case "following":
 				return "/api/posts/following";
+			case "posts":
+				return `/api/posts/user/${username}`;
+			case "likes":
+				return `/api/posts/likes/${userId}`;
 			default:
 				return "/api/posts/all";
 		}
@@ -20,13 +24,14 @@ const Posts = ({feedType }) => {
 	const {data:posts, isLoading, refetch, isRefetching } = useQuery({
 		queryKey: ["posts"],
 
+
 		queryFn: async () => {
 			try {
 				const res = await fetch(POST_ENDPOINT);
 				const data = await res.json();
 
 				if (!res.ok) {
-					throw new Error(data.error || "Something went wrong");
+					throw new Error(data.error || "Something went wrong!");
 				}
 				return data;
 			} catch (error) {
@@ -37,7 +42,7 @@ const Posts = ({feedType }) => {
 
 	useEffect(() => {
 		refetch();
-	}, [feedType, refetch]);
+	}, [feedType, refetch, username]);
 
 	return (
 		<>
